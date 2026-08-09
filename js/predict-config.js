6a986092e8a41880c0de7e5635cb328be8bb2462
wqh-account <2053839165@qq.com>
2026-08-09 14:00:27 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://harold-planes-capital-art.trycloudflare.com/api";
+window.PREDICT_API = "https://dog-pairs-after-aerial.trycloudflare.com/api";
