4fac38a12c8a99ec7587f93067c8f594816c72cb
wqh-account <2053839165@qq.com>
2026-08-11 14:00:51 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://west-coins-conflict-adjusted.trycloudflare.com/api";
+window.PREDICT_API = "https://gorgeous-genetic-oops-hugo.trycloudflare.com/api";
