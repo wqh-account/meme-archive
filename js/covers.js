adbcb6d098b28e5f33b53aea9817545adf8a6df2
wqh-account <2053839165@qq.com>
2026-08-24 08:00:42 +0800

    auto: js/covers.js

--- js/covers.js
+++ js/covers.js
@@ -5,10 +5,10 @@ window.COVERS = {
   "BV1G48M6XEBt": "images/covers/BV1G48M6XEBt.jpg",
   "BV1rHbY6MEB9": "images/covers/BV1rHbY6MEB9.jpg",
   "BV1rg8y6uEtG": "images/covers/BV1rg8y6uEtG.jpg",
+  "BV1j38B65Eyw": "images/covers/BV1j38B65Eyw.jpg",
+  "BV1Jv8p6kEJw": "images/covers/BV1Jv8p6kEJw.jpg",
   "BV1Vy8r6JE9z": "images/covers/BV1Vy8r6JE9z.jpg",
   "BV1db8x6VE73": "images/covers/BV1db8x6VE73.jpg",
-  "BV1Jv8p6kEJw": "images/covers/BV1Jv8p6kEJw.jpg",
   "BV1ES876dErp": "images/covers/BV1ES876dErp.jpg",
-  "BV1j38B65Eyw": "images/covers/BV1j38B65Eyw.jpg",
   "BV1GT8F6YEKY": "images/covers/BV1GT8F6YEKY.jpg",
 };
