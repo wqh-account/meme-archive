98c2ee378b9473b770204acf477c1c4b915fec1a
wqh-account <2053839165@qq.com>
2026-08-27 20:01:15 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://applicant-floral-touch-highlighted.trycloudflare.com/api";
+window.PREDICT_API = "https://arrested-threaded-massachusetts-unknown.trycloudflare.com/api";
