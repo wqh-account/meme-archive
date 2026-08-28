c80350637f9db82074b8913b4cca6f6a714fd8be
wqh-account <2053839165@qq.com>
2026-08-29 07:52:58 +0800

    auto: js/covers.js

--- js/covers.js
+++ js/covers.js
@@ -1,14 +1,14 @@
 /* 自动生成: py fetch-covers.py 可重新抓取 */
 /* B站封面已本地化: images/covers/{BV}.jpg，绕开防盗链 */
 window.COVERS = {
+  "BV1Jrtc6GEVU": "images/covers/BV1Jrtc6GEVU.jpg",
+  "BV1NC4Z6VEUQ": "images/covers/BV1NC4Z6VEUQ.jpg",
+  "BV1P6496fExM": "images/covers/BV1P6496fExM.jpg",
+  "BV137tc6ZExx": "images/covers/BV137tc6ZExx.jpg",
+  "BV1Go8f6PEHd": "images/covers/BV1Go8f6PEHd.jpg",
+  "BV1DNhw6iE1V": "images/covers/BV1DNhw6iE1V.jpg",
   "BV1N1tF6SE2S": "images/covers/BV1N1tF6SE2S.jpg",
-  "BV1XT4f6dED8": "images/covers/BV1XT4f6dED8.jpg",
-  "BV1CEhV6xEjG": "images/covers/BV1CEhV6xEjG.jpg",
-  "BV1JJtF6qEgn": "images/covers/BV1JJtF6qEgn.jpg",
   "BV1vm4f6bETN": "images/covers/BV1vm4f6bETN.jpg",
-  "BV1efhc66E29": "images/covers/BV1efhc66E29.jpg",
-  "BV1M3hw6aEbQ": "images/covers/BV1M3hw6aEbQ.jpg",
+  "BV1Jktc6rERm": "images/covers/BV1Jktc6rERm.jpg",
   "BV1bMhV6PEtE": "images/covers/BV1bMhV6PEtE.jpg",
-  "BV1rW4R6bEf9": "images/covers/BV1rW4R6bEf9.jpg",
-  "BV1Rs8d6tEJ1": "images/covers/BV1Rs8d6tEJ1.jpg",
 };
