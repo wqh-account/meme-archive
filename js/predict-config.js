be15d30056cd605463891465206dc8c301eac1aa
wqh-account <2053839165@qq.com>
2026-09-04 08:00:57 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://maui-gibson-kate-estate.trycloudflare.com/api";
+window.PREDICT_API = "https://translate-platform-refinance-smith.trycloudflare.com/api";
