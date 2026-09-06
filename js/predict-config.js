f7f8a87309af1c43197d67ba14a2f9ed536da363
wqh-account <2053839165@qq.com>
2026-09-06 20:01:54 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://screensavers-dame-fog-occurs.trycloudflare.com/api";
+window.PREDICT_API = "https://thrown-context-juvenile-bridges.trycloudflare.com/api";
