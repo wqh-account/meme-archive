ec2b747c4b02b191bd7f4243ee8451e00220242f
wqh-account <2053839165@qq.com>
2026-09-12 08:00:29 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://satisfactory-revisions-cartoon-our.trycloudflare.com/api";
+window.PREDICT_API = "https://somewhat-resolution-groundwater-snapshot.trycloudflare.com/api";
