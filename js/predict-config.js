4997af0a6ef07fb77423d22e4ff78ba5c2c4a26d
wqh-account <2053839165@qq.com>
2026-09-25 08:00:57 +0800

    auto: js/predict-config.js

--- js/predict-config.js
+++ js/predict-config.js
@@ -1,2 +1,2 @@
 ﻿// 预测 API 地址配置（由 health-check.ps1 / start-predict-tunnel.ps1 自动更新）
-window.PREDICT_API = "https://paragraphs-assessing-hierarchy-tale.trycloudflare.com/api";
+window.PREDICT_API = "https://knows-gpl-transmitted-amsterdam.trycloudflare.com/api";
